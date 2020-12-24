c0533dd940b172050cc9bcb9b5e5d106991adb3f
Erik Carlsson <info@code301.com>
2020-12-24 14:12:23 +0000

    Init

---
+++ lib/Dist/Zilla/Plugin/Author/CSSON/GithubActions.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Author::CSSON::GithubActions;
+
+# ABSTRACT: Short intro
+# AUTHORITY
+our $VERSION = '0.0100';
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::Author::CSSON::GithubActions;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::Author::CSSON::GithubActions is ...
+
+=head1 SEE ALSO
+
+=cut
