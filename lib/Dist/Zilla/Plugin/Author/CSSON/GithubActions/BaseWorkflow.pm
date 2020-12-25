7dbd54bf2a6fcb9193c7de66a99cad78e6a4be4a
Erik Carlsson <info@code301.com>
2020-12-25 18:19:40 +0000

    Bump version after 0.0100

--- lib/Dist/Zilla/Plugin/Author/CSSON/GithubActions/BaseWorkflow.pm
+++ lib/Dist/Zilla/Plugin/Author/CSSON/GithubActions/BaseWorkflow.pm
@@ -6,7 +6,7 @@ package Dist::Zilla::Plugin::Author::CSSON::GithubActions::BaseWorkflow;
 
 # ABSTRACT: An example Github Actions workflow
 # AUTHORITY
-our $VERSION = '0.0100';
+our $VERSION = '0.0101';
 
 use Moose;
 with qw/
